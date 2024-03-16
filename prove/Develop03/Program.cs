17399281f533b1a3cdad334b7c2e24aff1b2a5f1
Cattail411 <lilycattail@gmail.com>
2024-03-16 12:17:40 -0600

    scripture reader

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        class Scripture
+            {
+                Reference: Reference;
+                words: List<Word>;
+                Scripture(Reference reference, TextReader);
+                HideRandomWords(numberToHide: int);
+                GetDisplayText();
+                IsCompletelyHidden();
+            }
+        class Reference
+            {   
+                Book: string;
+                Chapter: int;
+                Verse: int;
+                endVerse: int;
+                Reference(string book, int chapter, int verse, int endVerse);
+                Reference(string book, int chapter, int startVerse, int endVerse);
+                GetDisplayText();
+            }
+        class Word
+            {
+                Text: string;
+                IsHidden: bool;
+                Word(TextReader);
+                Hide();
+                Show();
+                IsHidden();
+                GetDisplayText();
+            }
+    Console.clear;
     }
 }
