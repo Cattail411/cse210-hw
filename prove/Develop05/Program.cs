a5f0d6c350a4ae4da2a0ee81c320d40587cb2992
Cattail411 <lilycattail@gmail.com>
2024-03-30 13:40:46 -0600

    changes

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.ComponentModel;
 
-class Program
+class GoalManager
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop05 World!");
+    public List<Goal> Goals { get; set; }
+    start();
+    DisplayPlayerInfo();
+    ListGoalNames();
+    ListGoalDetails();
+    CreateGoal();
+    record();
+    SaveGoals();
+    LoadGoals();
+}
+class SimpleGoal
+{
+    isComplete = false;
+    SimpleGoal(name, DescriptionAttribute, points){
+        RecordEvent();
+        IsComplete();
+        GetStringRepresntation();
+    }
+}
+class EternalGoal
+{
+    EternalGoal(name, description) : base(name, description) {
+        RecordEvent();
+        IsComplete();
+        GetStringRepresntation();
+    }
+}
+class ChecklistGoal
+{
+    amountCompleted = 0;
+    targetAmount = 0;
+    bonusPoints = 0;
+    ChecklistGoal(name, description, targetAmount, bonusPoints) : base(name, description) {
+        this.targetAmount = targetAmount;
+        this.bonusPoints = bonusPoints;
+        RecordEvent();
+        IsComplete();
+        GetStringRepresentation();
     }
 }
